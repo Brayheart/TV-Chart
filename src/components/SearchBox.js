8a88c24de778118ea8e98b70f1681879a365b0e4
Benjamin Mizrahi <benjajamin@gmail.com>
2018-04-08 21:25:11 +0300

    Seperated logic from SearchBox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import jsonp from 'jsonp';
 import puff from '../puff.svg';
 import ErrorModal from './ErrorModal';
 import Suggestions from './Suggestions';
-import config from '../firebaseConfig'
-var firebase = require("firebase");
-firebase.initializeApp(config);
-var database = firebase.database();
+import {updateRatings} from '../utils';
 
-const omdbPrefix= "https://www.omdbapi.com/";
+
 
 class SearchBox extends Component{
   constructor(props){
@@ -28,7 +23,13 @@ class SearchBox extends Component{
     return (
     <div>
     <form className="form-inline" onSubmit={this.handleSubmit}>
-      <input id={this.props.wide? "wideInput": "navInput"} className="form-control mr-sm-2" type="search" placeholder="Search for a TV show" value={this.state.value} aria-label="Search" onChange={this.handleChange} disabled={this.state.disabled}/>
+      <input id={this.props.wide? "wideInput": "navInput"}
+      className="form-control mr-sm-2"
+      type="search"
+      placeholder="Search for a TV show"
+      value={this.state.value}
+      aria-label="Search" onChange={this.handleChange} disabled={this.state.disabled}
+      />
       {this.state.loading? loadingIcon: button}
     </form>
     {this.props.wide? suggestions: ""}
@@ -39,12 +40,13 @@ class SearchBox extends Component{
 
   handleChange(event){
     this.setState({value:event.target.value});
+
   }
 
   handleSubmit(event){
     event.preventDefault();
     this.setState({disabled:true});
-    this.updateRatings(this.state.value);
+    updateRatings(this.state.value, this);
   }
   componentWillReceiveProps(nextProps){
     if(this.props!=nextProps)
@@ -54,7 +56,7 @@ class SearchBox extends Component{
   componentDidMount(){
     if(this.props.pathname != "" && this.props.pathname!=undefined){
       this.setState({value: this.props.pathname});
-      this.updateRatings(this.props.pathname);
+      updateRatings(this.props.pathname, this);
     }
     
   }
@@ -67,81 +69,8 @@ class SearchBox extends Component{
 
   handleSuggestionClick(value){
     this.setState({value: value, disabled:true});
-    this.updateRatings(value);
-  }
-
-  updateRatings(series){
-    this.setState({loading:true});
-    database.ref('/'+series.toLowerCase()).once('value').then(snap =>{
-      if(!snap.exists()){
-        this.getRatingsFromOMDB(series);
-        return;
-      }
-      var data=snap.val();
-      this.props.updateMethod(data.title, data.seasons);
-    });
-  }
-
-  getRatingsFromOMDB(series){
-    var url=omdbPrefix+"?t="+series+"&type=series&apikey="+process.env.REACT_APP_OMDB_KEY;
-    jsonp(url, null, (function (err, data) {
-      if (err) {
-        this.setState({disabled:false, error: true, loading:false, errorMsg: "Can't access the server. Please try again later."});
-          return;
-      } else {
-       if(data.Title===undefined || data.totalSeasons== "N/A"){   
-          this.setState({disabled:false, error: true, loading:false, errorMsg: "Sorry, couldn't find that series :("});        
-          return;
-        }
-        this.getSeasons(data.Title, parseInt(data.totalSeasons));
-      }
-    }).bind(this));
-  }
-
-  getSeasons(series,numOfSeasons){
-    const seasons=[];
-    var promises=[];
-    for(var i=1;i<=numOfSeasons;i++){
-        (function(i, getEpisodesRatings){
-        var url=omdbPrefix+"?t="+series+"&Season="+i+"&apikey="+process.env.REACT_APP_OMDB_KEY;
-        var getSeasonEpisodes=axios.get(url).then(result=>{
-          seasons[i-1]=getEpisodesRatings(result.data, i);
-          //console.log("season "+i+" rating: "+seasons[i-1]);
-        },error=>{
-          //deal with error.
-        });
-        promises.push(getSeasonEpisodes);
-      })(i,this.getEpisodesRatings);
-
-    }
-    Promise.all(promises).then(results=>{
-      this.setState({disabled:false, loading:false});
-      var data={title:series ,seasons};
-      this.addSeriesToFirebase(series.toLowerCase(), data);
-      this.props.updateMethod(series, seasons);
-      window.history.pushState({}, series+" Ratings", "/"+series);
-      
-    },error=>{
-        this.setState({disabled:false, error: true, loading:false, errorMsg: "Can't access the server. Please try again later."});
-        return;
-    });
-  }
-
-  getEpisodesRatings(data, seasonNum){
-    var ratings=[];
-    var episodes=data.Episodes;
-    for(var i=1;i<=episodes.length;i++){
-      var rating=parseFloat(episodes[i-1].imdbRating);
-      if(!isNaN(rating))
-        ratings.push({name: episodes[i-1].Title, rating: rating, season: seasonNum, episode: i});
-    }
-    //console.log("ratings: "+ratings)
-    return ratings;
-  }
-
-  addSeriesToFirebase(series, data){
-    database.ref(series).set(data);
-  }
+    updateRatings(value, this);
+  } 
 
 }
 
---
+++ src/utils.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import jsonp from 'jsonp';
+import config from './firebaseConfig';
+
+var firebase = require("firebase");
+firebase.initializeApp(config);
+var database = firebase.database();
+
+const omdbPrefix= "https://www.omdbapi.com/";
+
+export function updateRatings(series, component){
+  component.setState({loading:true});
+  database.ref('/'+series.toLowerCase()).once('value').then(snap =>{
+  if(!snap.exists()){
+    getRatingsFromOMDB(series, component);
+    return;
+  }
+  var data=snap.val();
+  component.props.updateMethod(data.title, data.seasons);
+  });
+}
+
+function getRatingsFromOMDB(series, component){
+    var url=omdbPrefix+"?t="+series+"&type=series&apikey="+process.env.REACT_APP_OMDB_KEY;
+    jsonp(url, null, (function (err, data) {
+      if (err) {
+        component.setState({disabled:false, error: true, loading:false, errorMsg: "Can't access the server. Please try again later."});
+          return;
+      } else {
+       if(data.Title===undefined || data.totalSeasons== "N/A"){   
+          component.setState({disabled:false, error: true, loading:false, errorMsg: "Sorry, couldn't find that series :("});        
+          return;
+        }
+        component.getSeasons(data.Title, parseInt(data.totalSeasons), component);
+      }
+    }));
+  }
+
+function getSeasons(series,numOfSeasons, component){
+    const seasons=[];
+    var promises=[];
+    for(var i=1;i<=numOfSeasons;i++){
+        (function(i){
+        var url=omdbPrefix+"?t="+series+"&Season="+i+"&apikey="+process.env.REACT_APP_OMDB_KEY;
+        var getSeasonEpisodes=axios.get(url).then(result=>{
+          seasons[i-1]=getEpisodesRatings(result.data, i);
+          //console.log("season "+i+" rating: "+seasons[i-1]);
+        },error=>{
+          //deal with error.
+        });
+        promises.push(getSeasonEpisodes);
+      })(i);
+
+    }
+    Promise.all(promises).then(results=>{
+      component.setState({disabled:false, loading:false});
+      var data={title:series ,seasons};
+      addSeriesToFirebase(series.toLowerCase(), data);
+      component.props.updateMethod(series, seasons);
+      window.history.pushState({}, series+" Ratings", "/"+series);
+      
+    },error=>{
+        component.setState({disabled:false, error: true, loading:false, errorMsg: "Can't access the server. Please try again later."});
+        return;
+    });
+  }
+
+function getEpisodesRatings(data, seasonNum){
+    var ratings=[];
+    var episodes=data.Episodes;
+    for(var i=1;i<=episodes.length;i++){
+      var rating=parseFloat(episodes[i-1].imdbRating);
+      if(!isNaN(rating))
+        ratings.push({name: episodes[i-1].Title, rating: rating, season: seasonNum, episode: i});
+    }
+    //console.log("ratings: "+ratings)
+    return ratings;
+}
+
+function addSeriesToFirebase(series, data){
+  database.ref(series).set(data);
+}
